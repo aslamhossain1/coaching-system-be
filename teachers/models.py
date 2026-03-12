f9b30a74ef727526690b0afabbb8b57ebeae7473
Aslam Hossain <aslamhossain.msl@gmail.com>
2026-03-12 19:17:18 +0600

    updated

--- teachers/models.py
+++ teachers/models.py
@@ -21,6 +21,7 @@ class Teacher(models.Model):
     batch = models.ForeignKey("teachers.Batch", on_delete=models.SET_NULL, null=True, blank=True, related_name="teachers")
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return self.full_name
 
